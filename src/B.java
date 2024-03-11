4726a3bf50966320ea24670e4af357aab9285a4b
wlsdn <bryan.kim0810@gmail.com>
2024-03-11 18:37:02 +0900

    B.java, C.java 추가 commit

--- src/B.java
+++ src/B.java
@@ -2,6 +2,7 @@ public class B {
 
     public static void main(String[] args) {
         System.out.println("B.java 입니다");
+        System.out.println("추가 입니다.");
     }
 
 }
--- src/C.java
+++ src/C.java
@@ -1,7 +1,9 @@
 public class C {
 
     public static void main(String[] args) {
+
         System.out.println("C.java 입니다");
+        System.out.println("추가입니다.");
     }
 
 }
